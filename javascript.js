efb56b634d5c3b251af42e2e99ee0ec6cd6d38b3
Lemuel <77911664+vnjodev@users.noreply.github.com>
2025-10-19 14:05:32 +0800

    Create function for computer choice
    Worked with Math.random to assign numbers to "rock",
    "paper" and "scissors" choices.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3)
+    if (choice === 0) {
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
